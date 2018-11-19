8f62b920c6e6fff2be71bdf9c39439bc5364357c
duanjiefei <jiefeiduan@163.com>
2018-11-20 00:16:22 +0800

    单例模式 懒汉式方法  测试实例

---
+++ src/Singleton/Test.java
@@ -0,0 +1,23 @@
+package Singleton;
+
+public class Test {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new T());
+        Thread t2 = new Thread(new T());
+        Thread t3 = new Thread(new T());
+        Thread t4 = new Thread(new T());
+        Thread t5 = new Thread(new T());
+        Thread t6 = new Thread(new T());
+        Thread t7 = new Thread(new T());
+        Thread t8 = new Thread(new T());
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+        t6.start();
+        t7.start();
+        t8.start();
+        System.out.println("main thread end");
+    }
+}
