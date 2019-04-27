74f7ab2f73f3b4047968569420077dfdbc14f051
duanjiefei <jiefeiduan@163.com>
2019-04-27 10:48:48 +0800

    单例模式  懒汉式的优缺点

--- src/Singleton/lazy/LazySingleton.java
+++ src/Singleton/lazy/LazySingleton.java
@@ -1,4 +1,4 @@
-package Singleton;
+package Singleton.lazy;
 
 public class LazySingleton {
 
@@ -11,7 +11,20 @@ public class LazySingleton {
      * 当遇到多线程时，懒汉式大概率会失效，会得到多个实体对象，不能够保证实体对象的唯一性
      * @return
      */
-    public static LazySingleton getInstance(){
+    public static  LazySingleton getInstance(){
+        if (lazySingleton == null){
+            lazySingleton = new LazySingleton();
+        }
+        return lazySingleton;
+    }
+
+    /**
+     * 优点 synchronized  能够保证  在多线程加载时，保证线程安全
+     *
+     * 缺点 每次加载时  都会加锁，降低效率
+     * @return
+     */
+    public static synchronized LazySingleton getInstance1(){
         if (lazySingleton == null){
             lazySingleton = new LazySingleton();
         }
